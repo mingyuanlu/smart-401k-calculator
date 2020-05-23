e12092784bb7d8da5cdbab1ea7f234d919350c42
Ming-Yuan Lu <michaelmylu@gmail.com>
2020-05-23 14:28:21 -0500

    Add first piece of code

---
+++ Smart401kCalculator.scala
@@ -0,0 +1,10 @@
+// Smart 401k Calculator - Ming-Yuan Lu
+
+
+object Smart401kCalculator extends App {
+   val usage = "scala Smart401kCalculator <Pretax income> <Current effective tax rate (%)> <Effective tax rate at retirement (%)> <Number of years to retirement
+   \n <Interest rate (%, default 5)> <Inflation (%, default 2)>"
+   if ( args.length != 6 ) println(usage) 
+
+
+}
